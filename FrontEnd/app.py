02742c784a02eebab7ef498cf69984d7037d79e6
Sarah Antunes de Sousa <sarahantunes40@gmail.com>
2023-04-19 21:55:10 -0300

    Criando a estrutura para organizar os arquivos do frontend e adicionado os arquivos da barra de navegacao funcionando
    Foi criada a pasta FrontEnd contendo o arquivo app.py para definir as rotas, e as pastas templates e static, com a primeira mantendo os arquivos html do projeto, sendo a base.html responsavel pela criacao e repeticao da barra de navegacao nas outras paginas, e a ultima as pastas css e img, contendo os arquivos css e de imagens respectivamente.

---
+++ FrontEnd/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home(): 
+    return render_template("index.html")
+
+@app.route("/consultar")
+def consultar():
+    return render_template("consultar.html")
+
+@app.route("/gastos")
+def gastos():
+    return render_template("gastos.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
